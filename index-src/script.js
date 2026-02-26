2d59ad5c4d2d7c57fcd22f801eed0d6e4f763a3c
Minecube <126375609+Minecube1510@users.noreply.github.com>
2026-02-26 11:18:33 +0000

    [Dev] Build MD Checker Web - 2.1

--- index-src/script.js
+++ index-src/script.js
@@ -7,40 +7,81 @@
 /* Imports */
 import * as jsFunc
     from "./scripts/func.js";
+import * as jsVar
+    from "./scripts/var.js";
+//
 import * as get_MD
     from "./scripts/get-md.js";
 
 
-/* Get MD */
-//.
-
-
 /* Setup */
+    // Isi Konten MD //
 const content_MD = ((jsFunc).js_GetId("check-md"));
 //
-//?
-
-
-/* Input Dynamic - Get MD */
-//?
-const md_Name = ((get_MD).thisGet_MD("README"));
-//?
-const md_Linker = (``);
+    // Section Link MD //
+const mdc_Input = ((jsFunc).js_GetId("mdc-to-search"));  // Input
+const mdc_Btn = ((jsFunc).js_GetId("mdc-to-check"));  // Button
+//
+const mdc_Ceklink = ((jsFunc).js_GetId("mdc-ceklink"));  // Ngecek link-nya
 
 
 /* Execute */
-function check_Address_MD () {
-    console.log(
-        (`Linking MD:\n`),
-        (md_Name)
-    );
-}
-//
 function check_File_MD () {
-    fetch(md_Name).then(
-        res => ((res).text())
-    ).then(md => {
+    /* Act: Check */
+    const file_md = ((mdc_Input).value.trim());
+    if (!(file_md)) {
+        //console.log("Nor the MarkDown file...");
+        return;
+    }
+    //
+    const getFile = ((get_MD).thisGet_MD(file_md));
+    if (
+        (!(getFile)) ||
+        (getFile) === (jsVar.js_Blank)
+    ) {
+        console.log("File is Empty OR Not-Valid.");
+        return;
+    }
+
+    /* Act: Execute */
+    const err_2 = (`Markdown File is Not-Found 404`);
+    //
+    fetch(getFile)
+    .then(res => {
+        if (!((res).ok)) {
+            throw new Error(err_2);
+        }
+        return ((res).text());
+    })
+    .then(md => {
+        (mdc_Ceklink).innerHTML = ((jsFunc)
+        .draw_Html(("a"), [(`target::_blank`), (`draggable::false`),
+            (`class::mdc-link`), (`href::${getFile}`),
+        ], (`${file_md}.md`)));
+        //
         (content_MD).innerHTML = ((marked).parse(md));
+    })
+    .catch(err => {
+        //console.error(err);
+        //
+        (mdc_Ceklink).style.color = ("var(--tw-red-600)");  /* text-red-600 */
+        (mdc_Ceklink).innerHTML = (err_2);
+        //
+        (content_MD).innerHTML = (jsFunc)
+        .draw_Html(("p"), [
+            (`class::mdc-err`),
+        ], (`${err.message}`));
+    });
+}
+
+
+/* Execute */
+function exc_Check_MD () {
+    (mdc_Btn).addEventListener(("click"), (check_File_MD));
+    (mdc_Input).addEventListener(("keydown"), (e) => {
+        if (((e).key) === ("Enter")) {
+            check_File_MD();
+        }
     });
 }
 
@@ -48,7 +89,7 @@ function check_File_MD () {
 /* Uji Coba */
 //check_Address_MD ();
 //
-check_File_MD ();
+exc_Check_MD ();
 
 
 //-//
---
+++ index-src/scripts/var.js
@@ -0,0 +1,13 @@
+/*
+/
+    index-src/scripts/var.js
+/
+*/
+
+
+/* Variables */
+export const js_Blank = (``);
+export const js_Space = (` `);
+
+
+//-//
